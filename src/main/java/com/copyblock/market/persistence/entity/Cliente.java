2b65c80d388662f3370dccb8cde9ebf0a1a18a39
Victor Osorio <vimosorioga@unal.edu.co>
2023-02-12 21:23:10 -0500

    Entities and relations mapping

---
+++ src/main/java/com/copyblock/market/persistence/entity/Cliente.java
@@ -0,0 +1,75 @@
+package com.copyblock.market.persistence.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+
+    @Id
+    @Column(name = "id")
+    private String idCliente;
+
+    private String nombre;
+
+    @Column(name = "apellidos")
+    private String apellido;
+
+    private Long celular;
+    private String direccion;
+
+    @Column(name = "correo_electronico")
+    private String email;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Compra> compras;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Long getCelular() {
+        return celular;
+    }
+
+    public void setCelular(Long celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
