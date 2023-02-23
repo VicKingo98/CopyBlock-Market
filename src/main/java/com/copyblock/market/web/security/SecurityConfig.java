f72b78c41514e742b646031410e65b399d4a61b5
Victor Osorio <vimosorioga@unal.edu.co>
2023-02-23 13:36:36 -0500

    Authentication by using Json Web Token. Classes configuration and controller including to show it in the api

--- src/main/java/com/copyblock/market/web/security/SecurityConfig.java
+++ src/main/java/com/copyblock/market/web/security/SecurityConfig.java
@@ -1,23 +1,36 @@
 package com.copyblock.market.web.security;
 
+import com.copyblock.market.domain.service.CopyBlockUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private CopyBlockUserDetailsService copyBlockUserDetailsService;
 
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .userDetailsService(userDetailsService)
+                .userDetailsService(copyBlockUserDetailsService)
                 .and()
                 .build();
     }
-    
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable().authorizeRequests()
+                .antMatchers("/**/authenticate").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .build();
+    }
 }
